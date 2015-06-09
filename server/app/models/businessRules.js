84b2fa9d1f8ea400ed25549b0e44e07d314b7e12
Tim Hartemink <ta.hartemink@gmail.com>
2015-06-09 21:37:08 +0200

    Used middleware pre hook to validate model and property properties of businessRule model

--- server/app/models/businessRules.js
+++ server/app/models/businessRules.js
@@ -8,7 +8,9 @@
     var mongoose = require('mongoose'),
         Schema = mongoose.Schema,
         schemaName,
-        modelName;
+        modelName,
+        errMsg;
+
 
     /**
      * Business Rule schema
@@ -29,7 +31,7 @@
     );
 
     //TODO: Validation of model and property. Needs check on database to see if model and property exist.
-    schemaName.path('model').validate(function (val) {
+    /*schemaName.path('model').validate(function (val) {
         try {
             return (mongoose.model(val) !== null && mongoose.model(val) !== undefined);
         } catch (err) {
@@ -37,6 +39,28 @@
             return false;
         }
     }, 'Model does not exist!');
+
+    schemaName.path('property').validation(function (val) {
+        return true;
+    }, 'Property does not exist!');*/
+
+    schemaName.pre('validate', function(next) {
+        try {
+            if (mongoose.model(this.model) !== null && mongoose.model(this.model) !== undefined) {
+                if (mongoose.model(this.model).schema.path(this.property) !== null && mongoose.model(this.model).schema.path(this.property) !== undefined) {
+                    next();
+                }
+            }
+            else {
+                errMsg = new Error('Model and/or property are nonexistent!');
+                next(errMsg);
+            }
+        } catch (err) {
+            console.log(err.message);
+            errMsg = new Error('Model and/or property are nonexistent!');
+            next(errMsg);
+        }
+    });
 
     modelName = "BusinessRule";
     mongoose.model(modelName, schemaName);
